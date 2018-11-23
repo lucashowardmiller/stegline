c452f9747f366924f8fca779d365f5c6833b5b02
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-11-22 23:41:11 -0600

    exif is done, less outdir support

--- metaModule.py
+++ metaModule.py
@@ -7,18 +7,18 @@ import exifread
 
 
 def metadata (input_file, output_folder):
+    #this is on the backburner for a bit
+    #not very ctf nor super flashy
    stat_info = os.stat(input_file)
 
    print()
 def exif(input_file, output_folder):
   img = open(input_file, 'rb')
   tags = exifread.process_file(img)
-  #TODO format exif strings and create a report
-
+  #TODO get it to respect folders
 
   f = open("report.txt", "w+")
+  f.write("Exif Data: \n")
 
   for tag in tags.keys():
-      if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-
-          f.write("Key: %s, value %s" % (tag, tags[tag]) + "\n")
+    f.write("Key: %s, value %s" % (tag, tags[tag]) + "\n")
