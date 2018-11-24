1bdb23250914344cb12847fc2ef84e1517fa0ef3
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-11-24 14:57:32 -0600

    finalish file mod, until text search is added and extraction (with binwalk api)

--- fileModule.py
+++ fileModule.py
@@ -1,11 +1,5 @@
 import os
 import binwalk
-import subprocess
-from subprocess import call
-
-# todo find python bindings
-
-
 
 def filecheck(input_file, output_folder):
     file_extension = os.path.splitext(input_file)[1]
@@ -17,19 +11,23 @@ def filecheck(input_file, output_folder):
             print((result.description))
             fileList.append(result.description)
 
-    # matching binwalk filetypes to real filetypes looks like effort
+    # matching binwalk filetypes to real filetypes looks like effort (dict/tuples?)
     # current version relies on the quality of people reading it
     reportLocation = output_folder + '/' + 'report.txt'
     f = open(reportLocation, "w+")
     f.write("File data: \n")
     f.write("The os provided extension is " + file_extension + "\n")
-    if fileList.__sizeof__() > 0:
+    if len(fileList) > 0:
         f.write("Binwalk identified the file as:  " + fileList[0] + "\n")
     else:
         f.write("Binwalk did not identify any magic bytes." + "\n")
 
-
-
-
+    if len(fileList) > 1:
+        f.write("Binwalk identified " + str(len(fileList) - 1) + " potential hidden file(s)." + "\n")
+        # Needed to skip fileList[0], so this is what happended
+        i = 1
+        while i < len(fileList):
+            f.write("Hidden file " + str(i) + " " + fileList[i] + "\n")
+            i += 1
 
     print("end of filecheck")
