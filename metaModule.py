a83d3fccc7fdb1c3ba8c95b77489edde4744232f
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-11-24 23:42:01 -0600

    added start of strings() and some commenting

--- metaModule.py
+++ metaModule.py
@@ -1,10 +1,11 @@
-# TODO writ#TODO write metadata handler
-# TODO "write" exif handler
-
+#used for mkdir
 import os
-from PIL import Image
+#used to get exif data in exif()
 import exifread
-
+#used to call strings in strings()
+import subprocess
+#used for iterating through strings-ouput.txt to look for .{.} and NCL/SKY
+import re
 
 def metadata(input_file, output_folder):
     # this is on the backburner for a bit
@@ -24,3 +25,21 @@ def exif(input_file, output_folder):
     for tag in tags.keys():
         f.write("Key: %s, value %s" % (tag, tags[tag]) + "\n")
     f.write("\n")
+
+
+def strings(input_file, output_folder):
+    reportLocation = output_folder + '/' + 'report.txt'
+    extractionFolder = output_folder + '/' + 'SteglineGenerated'
+    stringLocation = extractionFolder + '/' + 'strings-output.txt'
+
+    if not os.path.exists(extractionFolder):
+        os.mkdir(extractionFolder)
+        print("Directory ExtractedFiles created ")
+    else:
+        print("Directory ExtractedFiles already exists")
+
+    subprocess.check_output(["strings", input_file, '>', stringLocation])
+    r = open(stringLocation, "r")
+    r.close()
+
+    print("end of strings")
