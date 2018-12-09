c91e182ba08e721cc19b8e4b4b52d2f4816fd399
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-12-09 17:17:52 -0600

    strings functions correctly

--- metaModule.py
+++ metaModule.py
@@ -32,8 +32,9 @@ def exif(input_file, output_folder):
 def strings(input_file, output_folder):
     # used to match ABCD{FLAG_w@CkY} (not currently used)
     flag_brackets = re.compile('.*{.*}')
-    #used to match NCL/SKY
-    flag_skyncl = re.compile('[NCL,SKY]{3}-[a-zA-Z]{4}-[0-9]{4}')
+    # used to match NCL/SKY, can false match with something like NKL-ABCD-1234
+    flag_skyncl = re.compile('[NCLSKY]{3}-[a-zA-Z]{3}-[0-9]{4}')
+
 
     reportLocation = output_folder + '/' + 'report.txt'
     extractionFolder = output_folder + '/' + 'SteglineGenerated'
@@ -42,24 +43,25 @@ def strings(input_file, output_folder):
 
     if not os.path.exists(extractionFolder):
         os.mkdir(extractionFolder)
-        print("Directory ExtractedFiles created ")
-    else:
-        print("Directory ExtractedFiles already exists")
 
-    subprocess.check_output(["strings", input_file, '>', stringLocation])
-    with open(stringLocation) as sL:
-        bracketMatch = re.findall('.*{.*}', sL.read())
-        skynclMatch = re.findall('[NCL,SKY]{3}-[a-zA-Z]{4}-[0-9]{4}', sL.read())
+    stingLocation = open(stringLocation, "w")
+
+    subprocess.call(["strings", input_file], stdout=stingLocation)
+    with open(stringLocation, 'r') as sL:
+        skynclMatch = re.findall(flag_skyncl, sL.read())
+        bracketMatch = re.findall(flag_brackets, sL.read())
     sL.close()
 
     f = open(reportLocation, "a+")
-    f.write("Potential flags found using Strings:" + "\n")
+    if len(bracketMatch) > 0 or len(skynclMatch) > 0:
+        f.write("\n" + "Potential flags found using Strings:" + "\n")
 
-    for match in bracketMatch:
-        f.write("Possible Flag: " + match)
+        for match in bracketMatch:
+            f.write("Possible Flag: " + match)
 
-    for match in skynclMatch:
-        f.write("Possible Flag: " + match)
-
+        for match in skynclMatch:
+            f.write("Possible Flag: " + match)
+    else:
+        f.write("\n" + "No flags in SKY/NCL format or .*{.*} format were found")
     f.close()
     print("end of strings")
