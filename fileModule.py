64c83fc1cdcb1f13d47a0ab2210f9ed73ce79a49
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-12-09 18:04:12 -0600

    changed how report open works to prevent clobering the other modules

--- fileModule.py
+++ fileModule.py
@@ -6,7 +6,7 @@ def filecheck(input_file, output_folder):
     file_extension = os.path.splitext(input_file)[1][1:]
     fileList = []
     #.jpg first JPEG used to verify between file_extension
-    extensionToFile = [('.jpg','JPEG'), ('.tif','TIFF'), ('.gif','GIF'), ('.tif','TIFF'), ('.tif','TIFF') ]
+    extensionToFile = [('.jpg', 'JPEG'), ('.tif', 'TIFF'), ('.gif', 'GIF'), ('.tif',' TIFF'), ('.tif', 'TIFF')]
 
     for module in binwalk.scan(input_file, signature=True, quiet=True):
         # print("%s Results:" % module.name)
@@ -17,7 +17,7 @@ def filecheck(input_file, output_folder):
     # matching binwalk filetypes to real filetypes looks like effort (dict/tuples?)
     # current version relies on the quality of people reading it
     reportLocation = output_folder + '/' + 'report.txt'
-    f = open(reportLocation, "w+")
+    f = open(reportLocation, "a+")
     f.write("File data: \n")
     f.write("The os provided extension is " + file_extension + "\n")
     if len(fileList) > 0:
@@ -27,7 +27,7 @@ def filecheck(input_file, output_folder):
 
     if len(fileList) > 1:
         f.write("Binwalk identified " + str(len(fileList) - 1) + " potential hidden file(s)." + "\n")
-        # Needed to skip fileList[0], so this is what happended
+        # Needed to skip fileList[0], so this is what happened
         i = 1
         while i < len(fileList):
             f.write("Hidden file " + str(i) + " " + fileList[i] + "\n")
