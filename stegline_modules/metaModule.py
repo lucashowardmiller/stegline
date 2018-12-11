f5e95359d800be0143f8c54a6bf5590dcb8eec9d
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2018-12-10 19:44:02 -0600

    strings is now fully functional

--- stegline_modules/metaModule.py
+++ stegline_modules/metaModule.py
@@ -23,7 +23,9 @@ def exif(input_file, output_folder):
     f.write("Exif Data: \n")
 
     for tag in tags.keys():
-        f.write("Key: %s, value %s" % (tag, tags[tag]) + "\n")
+        # limits really verbose tags
+        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+            f.write("%s, value %s" % (tag, tags[tag]) + "\n")
     f.write("\n")
 
     f.close()
@@ -33,7 +35,9 @@ def strings(input_file, output_folder):
     # used to match ABCD{FLAG_w@CkY}
     flag_brackets = re.compile('.*{.*}')
     # used to match NCL/SKY, can false match with something like NKL-ABCD-1234
-    flag_skyncl = re.compile('[NCLSKYnclsky]{3}-[a-zA-Z]{3}-[0-9]{4}')
+    flag_skyncl = re.compile('[NCLSKYnclsky]{3}-[a-zA-Z]{4}-[0-9]{4}')
+    # hactober/generic, can false match with something like glaf-dkskdjsk
+    flag_generic = re.compile('[FLAGflag]{4}-.*')
 
 
     reportLocation = output_folder + '/' + 'report.txt'
@@ -50,17 +54,22 @@ def strings(input_file, output_folder):
     with open(stringLocation, 'r') as sL:
         skynclMatch = re.findall(flag_skyncl, sL.read())
         bracketMatch = re.findall(flag_brackets, sL.read())
+        genericMatch = re.findall(flag_generic, sL.read())
     sL.close()
 
     f = open(reportLocation, "a+")
-    if len(bracketMatch) > 0 or len(skynclMatch) > 0:
+    if len(bracketMatch) > 0 or len(skynclMatch) > 0 or len(genericMatch) > 0:
         f.write("\n" + "Potential flags found using Strings:" + "\n")
 
         for match in bracketMatch:
             f.write("Possible CTF Flag: " + match)
 
+        for match in genericMatch:
+            f.write("Possible CTF Flag: " + match)
+
         for match in skynclMatch:
             f.write("Possible NCL Flag: " + match)
     else:
+        f.write("\n" + "Strings results: ")
         f.write("\n" + "No flags in SKY/NCL format or .*{.*} format were found")
     f.close()
