5231a5d1bd28a6a4eab3f8330ef944f7b38d57ad
lucashowardmiller <Lucas.h.miller@ndsu.edu>
2019-01-01 04:51:47 -0600

    refactored to use string_id method

--- stegline_modules/metaModule.py
+++ stegline_modules/metaModule.py
@@ -6,13 +6,14 @@ import exifread
 import subprocess
 # used for iterating through strings-ouput.txt to look for .{.} and NCL/SKY
 import re
+from stegline_modules import string_id
+
 
 def metadata(input_file, output_folder):
     # this is on the backburner for a bit
     # not very ctf nor super flashy
     # also pretty jank
     stat_info = os.stat(input_file)
-
 
 
 def exif(input_file, output_folder):
@@ -32,6 +33,7 @@ def exif(input_file, output_folder):
 
 
 def strings(input_file, output_folder):
+    # TODO refactor with new id all ctf strings method once completed
     # used to match ABCD{FLAG_w@CkY}
     flag_brackets = re.compile('.*{.*}')
     # used to match NCL/SKY, can false match with something like NKL-ABCD-1234
@@ -40,11 +42,9 @@ def strings(input_file, output_folder):
     # hactober/generic, can false match with something like glaf-dkskdjsk
     flag_generic = re.compile('[FLAGflag]{4}-.*')
 
-
     reportLocation = output_folder + '/' + 'report.txt'
     extractionFolder = output_folder + '/' + 'SteglineGenerated'
     stringLocation = extractionFolder + '/' + 'strings-output.txt'
-
 
     if not os.path.exists(extractionFolder):
         os.mkdir(extractionFolder)
@@ -53,23 +53,18 @@ def strings(input_file, output_folder):
 
     subprocess.call(["strings", input_file], stdout=stingLocation)
     with open(stringLocation, 'r') as sL:
-        skynclMatch = re.findall(flag_skyncl, sL.read())
-        bracketMatch = re.findall(flag_brackets, sL.read())
-        genericMatch = re.findall(flag_generic, sL.read())
+        string_file_lines = sL.readlines()
     sL.close()
 
+    matched_strings = string_id.return_ctf_flags(string_file_lines)
+
     f = open(reportLocation, "a+")
-    if len(bracketMatch) > 0 or len(skynclMatch) > 0 or len(genericMatch) > 0:
+    if len(matched_strings) > 0:
         f.write("\n" + "Potential flags found using Strings:" + "\n")
 
-        for match in bracketMatch:
+        for match in matched_strings:
             f.write("Possible CTF Flag: " + match)
 
-        for match in genericMatch:
-            f.write("Possible CTF Flag: " + match)
-
-        for match in skynclMatch:
-            f.write("Possible NCL Flag: " + match)
     else:
         f.write("\n" + "Strings results: ")
         f.write("\n" + "No flags in SKY/NCL format or .*{.*} format were found")
